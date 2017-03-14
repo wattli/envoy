bf059aa1962c178040250e8974db02cb28d54356
Tao Li <lita@google.com>
2017-03-14 12:49:09 -0700

    test

--- test/common/network/listen_socket_impl_test.cc
+++ test/common/network/listen_socket_impl_test.cc
@@ -9,18 +9,18 @@ TEST(ListenSocket, All) {
   listen(socket1.fd(), 0);
   EXPECT_EQ(15000U, socket1.localAddress()->ip()->port());
 
-  TcpListenSocket socket2("tcp://127.0.0.1:15000", true);
+  TcpListenSocket socket2("tcp://127.0.0.1:15002", true);
   listen(socket2.fd(), 0);
-  EXPECT_EQ("127.0.0.1:15000", socket2.localAddress()->asString());
+  EXPECT_EQ("127.0.0.1:15002", socket2.localAddress()->asString());
 
   EXPECT_THROW(Network::TcpListenSocket socket3(uint32_t(15000), true), EnvoyException);
-  EXPECT_THROW(Network::TcpListenSocket socket4("tcp://127.0.0.1:15000", true), EnvoyException);
+  EXPECT_THROW(Network::TcpListenSocket socket4("tcp://127.0.0.1:15002", true), EnvoyException);
 
   TcpListenSocket socket5(dup(socket1.fd()), 15000);
   EXPECT_EQ(15000U, socket5.localAddress()->ip()->port());
 
-  TcpListenSocket socket6(dup(socket1.fd()), std::string("tcp://127.0.0.1:15000"));
-  EXPECT_EQ("127.0.0.1:15000", socket6.localAddress()->asString());
+  TcpListenSocket socket6(dup(socket1.fd()), std::string("tcp://127.0.0.1:15004"));
+  EXPECT_EQ("127.0.0.1:15004", socket6.localAddress()->asString());
 }
 
 } // Network
--- test/server/configuration_impl_test.cc
+++ test/server/configuration_impl_test.cc
@@ -102,7 +102,7 @@ TEST(ConfigurationImplTest, DefaultListenerPerConnectionBufferLimit) {
   {
     "listeners" : [
       {
-        "port" : 1234,
+        "address": "tcp://127.0.0.1:1234",
         "filters": []
       }
     ],
